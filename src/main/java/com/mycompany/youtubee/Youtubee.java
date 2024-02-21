4082dfcd96ce136694bd307099a0008ef0ba779e
bdesi <bdesi@desibet>
2024-02-20 20:56:35 -0600

    video usuario parte 2 para Profesor Alfonso

--- src/main/java/com/mycompany/youtubee/Youtubee.java
+++ src/main/java/com/mycompany/youtubee/Youtubee.java
@@ -6,9 +6,15 @@ package com.mycompany.youtubee;
 public class Youtubee {
 
     public static void main(String[] args) {
-        Usuario usuario= new Usuario("Juan Iran GP");
+       Usuario usuario= new Usuario("Juan Iran GP");
        Video video= new Video ("Aprendiendo Java pt.2",1705,"https;//yotube");
        usuario.subirVideo(video);
        usuario.mostrarInfoUsuario();
+       
+       Usuario usuario2=new Usuario("anota a quien quieras");
+       Comentario comment1=new Comentario("anota algo que quieras ",usuario2);
+       System.out.println("El usuario "+ usuario2.getNombre()+"Publico el comentario "+ comment1.getContenido());
+       
+
     }
 }
